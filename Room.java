62572259acd41a5e97eaf1411687cfdd14cb02a1
Abdullah Saeed <saeedabdullahlyh@gmail.com>
2026-05-02 16:13:47 +0500

    Add Room class with properties and constructor

---
+++ Room.java
@@ -0,0 +1,11 @@
+class Room {
+    int id;
+    String type;
+    double price;
+
+    public Room(int id, String type, double price) {
+        this.id = id;
+        this.type = type;
+        this.price = price;
+    }
+}
