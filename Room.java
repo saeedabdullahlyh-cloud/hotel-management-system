adccfd4d5bb22489d367cfc3aa02e6f334d40e98
Abdullah Saeed <saeedabdullahlyh@gmail.com>
2026-05-02 18:34:51 +0500

    Update Room constructor to initialize price by type
    Modified Room constructor to set price based on room type.

--- Room.java
+++ Room.java
@@ -1,11 +1,19 @@
+package com.hotel;
 class Room {
     int id;
     String type;
     double price;
-
-    public Room(int id, String type, double price) {
-        this.id = id;
-        this.type = type;
-        this.price = price;
-    }
-}
+    public Room(int id, String type) {
+        this.id=id;
+        this.type=type;
+        if (type.equalsIgnoreCase("Single"))
+            this.price=50;
+        else if (type.equalsIgnoreCase("Double"))
+            this.price=100;
+        else if (type.equalsIgnoreCase("Sweet"))
+            this.price=200;   
+        else if (type.equalsIgnoreCase("Luxury"))
+            this.price=300;   
+        else
+            this.price=0;
+    }}
