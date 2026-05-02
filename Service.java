65fb0735604dea88532c0f77a5703fcfe6437972
Abdullah Saeed <saeedabdullahlyh@gmail.com>
2026-05-02 16:14:35 +0500

    Add Service class with properties and constructor

---
+++ Service.java
@@ -0,0 +1,11 @@
+class Service {
+    String name;
+    double price;
+    boolean isComplimentary;
+
+    public Service(String name, double price, boolean isComplimentary) {
+        this.name = name;
+        this.price = price;
+        this.isComplimentary = isComplimentary;
+    }
+}
