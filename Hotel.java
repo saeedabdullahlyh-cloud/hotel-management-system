c7f88730aced6492a0abee299a028f719fe31fed
Abdullah Saeed <saeedabdullahlyh@gmail.com>
2026-05-05 19:16:11 +0500

    Add booking and input validation methods

--- Hotel.java
+++ Hotel.java
@@ -27,3 +27,39 @@ class Hotel {
     public void addBooking(Booking b) {
         bookings.add(b);
     }}
+public int getValidInt(java.util.Scanner sc, String msg) {
+    while (true) {
+        System.out.print(msg);
+        if (sc.hasNextInt()) {
+            return sc.nextInt();
+        } else {
+            System.out.println("Invalid input!");
+            sc.next();
+        }
+    }
+    public void simpleBooking(java.util.Scanner sc) {
+    System.out.print("Enter Name: ");
+    sc.nextLine();
+    String name = sc.nextLine();
+    int roomId = getValidInt(sc, "Enter Room ID: ");
+    Room selected = null;
+    for (Room r : rooms) {
+        if (r.id == roomId) {
+            selected = r;
+            break;
+        }
+    }
+    if (selected == null) {
+        System.out.println("Invalid Room ID!");
+        return;
+    }
+    java.time.LocalDate in=java.time.LocalDate.now();
+    java.time.LocalDate out=in.plusDays(1);
+    Booking b = new Booking(name, in, out);
+    b.addRoom(selected);
+
+    bookings.add(b);
+
+    System.out.println("Booking Done! ID: " + b.id);
+}
+}
