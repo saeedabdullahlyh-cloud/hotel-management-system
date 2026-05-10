d9cc4d883ff7c043d1ad8f7b3f755bf745475edb
Abdullah Saeed <saeedabdullahlyh@gmail.com>
2026-05-10 12:44:21 +0500

    Add SQL imports for database operations

--- Hotel.java
+++ Hotel.java
@@ -1,4 +1,7 @@
 package com.hotel;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
